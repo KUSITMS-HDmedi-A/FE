51f8cf14c95535857a08a9380835f260e29b91a8
PIYUJIN <yjyj020201@gmail.com>
2023-09-11 13:16:31 +0900

    Feat: MainActivity 설정
    bottom navigation, fragment로 mainActivity 설정
    
    Resolves: #1

--- app/src/main/java/com/kusitms/hdmedi/MainActivity.kt
+++ app/src/main/java/com/kusitms/hdmedi/MainActivity.kt
@@ -2,10 +2,29 @@ package com.kusitms.hdmedi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.kusitms.hdmedi.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initBinding()
+        setContentView(binding.root)
+        initNavigation()
+    }
+
+    private fun initBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun initNavigation() {
+        NavigationUI.setupWithNavController(binding.navBar, findNavController(R.id.nav_host))
     }
 }
