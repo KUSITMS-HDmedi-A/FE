ed38c4e612249831291bd2d0717653245b7a6858
Minju Kim <mindori1019@gmail.com>
2023-09-13 00:13:48 +0900

    feature: jwt token dataStore 의존성 주입 object 클래스 생성

---
+++ core/network/src/main/java/com/core/network/di/TokenDataStoreModule.kt
@@ -0,0 +1,28 @@
+package com.core.network.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TokenDataStoreModule {
+    private const val TOKEN_DATASTORE_NAME = "TOKEN_PREFERENCES"
+    private val Context.tokenDataStore by preferencesDataStore(TOKEN_DATASTORE_NAME)
+
+    @Provides
+    @Singleton
+    @Named("token_preferences")
+    fun provideTokenDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> = context.tokenDataStore
+
+}
