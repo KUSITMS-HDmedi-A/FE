3082c638efec55cf3637c39ee561dd33d507e418
Minju Kim <mindori1019@gmail.com>
2023-09-13 01:32:35 +0900

    feature: Network 통신하는 코드 구현 & api interface 생성

---
+++ core/network/src/main/java/com/core/network/ApiService.kt
@@ -0,0 +1,21 @@
+package com.core.network
+
+import retrofit2.http.POST
+
+interface ApiService {
+    /**
+     * adhd 진단
+     */
+
+    /**
+     * auth (회원)
+     */
+
+    /**
+     * child
+     */
+
+    /**
+     * medicine
+     */
+}
