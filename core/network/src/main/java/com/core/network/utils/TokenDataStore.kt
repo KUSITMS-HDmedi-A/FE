846edc5d3d9c15cb6031ef56b327ebe24d465a7e
Minju Kim <mindori1019@gmail.com>
2023-09-13 00:13:31 +0900

    feature: jwt token dataStore 파일에서 이름 지정 & object에서 key값 정의

--- core/network/src/main/java/com/core/network/utils/TokenDataStore.kt
+++ core/network/src/main/java/com/core/network/utils/TokenDataStore.kt
@@ -7,48 +7,49 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
+import javax.inject.Named
 
-class TokenManager @Inject constructor(
-    private val dataStore: DataStore<Preferences>
+class TokenDataStore @Inject constructor(
+    @Named("token_preferences")private val dataStore: DataStore<Preferences>
 ) {
-    companion object {
-        private val ACCESS_TOKEN_KEY = stringPreferencesKey("access_token")
-        private val REFRESH_TOKEN_KEY = stringPreferencesKey("refresh_token")
+    object PreferenceKey {
+        val ACCESS_TOKEN_KEY = stringPreferencesKey("ACCESS_TOKEN")
+        val REFRESH_TOKEN_KEY = stringPreferencesKey("REFRESH_TOKEN")
     }
 
     fun getAccessToken(): Flow<String?> {
         return dataStore.data.map { prefs ->
-            prefs[ACCESS_TOKEN_KEY]
+            prefs[PreferenceKey.ACCESS_TOKEN_KEY]
         }
     }
 
     suspend fun saveAccessToken(token: String){
         dataStore.edit { prefs ->
-            prefs[ACCESS_TOKEN_KEY] = token
+            prefs[PreferenceKey.ACCESS_TOKEN_KEY] = token
         }
     }
 
     suspend fun deleteAccessToken(){
         dataStore.edit { prefs ->
-            prefs.remove(ACCESS_TOKEN_KEY)
+            prefs.remove(PreferenceKey.ACCESS_TOKEN_KEY)
         }
     }
 
     fun getRefreshToken(): Flow<String?> {
         return dataStore.data.map { prefs ->
-            prefs[REFRESH_TOKEN_KEY]
+            prefs[PreferenceKey.REFRESH_TOKEN_KEY]
         }
     }
 
     suspend fun saveRefreshToken(token: String){
         dataStore.edit { prefs ->
-            prefs[REFRESH_TOKEN_KEY] = token
+            prefs[PreferenceKey.REFRESH_TOKEN_KEY] = token
         }
     }
 
     suspend fun deleteRefreshToken(){
         dataStore.edit { prefs ->
-            prefs.remove(REFRESH_TOKEN_KEY)
+            prefs.remove(PreferenceKey.REFRESH_TOKEN_KEY)
         }
     }
 
