71626ca50d42023eee919a6a379313b8067f58ba
PIYUJIN <yjyj020201@gmail.com>
2023-09-14 02:23:32 +0900

    Feat: 약관리_약등록_01 화면 구성 및 화면 전환 기능 구현
    [feature:medicine:ui] 약관리_약등록_01(목적) toolbar 구성 및 화면 전환 기능 구현
    
    Fixes: #4

---
+++ feature/medicine/ui/src/main/java/com/kusitms/hdmedi/ui/RegisterMedicinePurposeFragment.kt
@@ -0,0 +1,44 @@
+package com.kusitms.hdmedi.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.kusitms.hdmedi.ui.databinding.FragmentRegisterMedicinePurposeBinding
+
+class RegisterMedicinePurposeFragment : Fragment() {
+
+    lateinit var fragmentRegisterMedicinePurposeBinding: FragmentRegisterMedicinePurposeBinding
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        fragmentRegisterMedicinePurposeBinding = FragmentRegisterMedicinePurposeBinding.inflate(inflater)
+
+        fragmentRegisterMedicinePurposeBinding.run {
+            toolbarRegisterMedicinePurpose.run {
+                title = "약 등록"
+            }
+        }
+        return fragmentRegisterMedicinePurposeBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        fragmentRegisterMedicinePurposeBinding.run {
+            buttonRegisterMedicinePurposeNext.setOnClickListener {
+                navController.navigate(R.id.action_registerMedicinePurposeFragment_to_registerMedicineDateFragment)
+            }
+        }
+    }
+}
