bb4561f8b3a9a254fc5c3d7d0c31738d82acf97b
Minju Kim <mindori1019@gmail.com>
2023-09-14 19:23:12 +0900

    feature: 회원가입 버튼 UI 완성

--- feature/signin/ui/src/main/java/com/kusitms/hdmedi/feature/signin/ui/SigninFragment.kt
+++ feature/signin/ui/src/main/java/com/kusitms/hdmedi/feature/signin/ui/SigninFragment.kt
@@ -1,6 +1,8 @@
 package com.kusitms.hdmedi.feature.signin.ui
 
+import android.content.res.ColorStateList
 import com.core.common.BaseFragment
+import com.google.android.material.button.MaterialButton
 import com.kusitms.hdmedi.feature.signin.ui.databinding.FragmentSigninBinding
 
 class SigninFragment : BaseFragment<FragmentSigninBinding>(R.layout.fragment_signin) {
@@ -8,7 +10,21 @@ class SigninFragment : BaseFragment<FragmentSigninBinding>(R.layout.fragment_sig
     }
 
     override fun viewCreated() {
+        binding.lifecycleOwner = this
+        initView()
+    }
 
+    private fun initView() {
+        binding.btnKakaoLogin.apply {
+            txtContent = getString(R.string.login_kakao)
+            btn.setTextColor(requireContext().getColor(com.core.common.R.color.dark_kakao))
+            val color = requireContext().getColor(com.core.common.R.color.yellow_kakao)
+            btn.backgroundTintList = ColorStateList.valueOf(color)
+            btn.icon = requireContext().getDrawable(R.drawable.ic_kakao)
+            btn.iconGravity = MaterialButton.ICON_GRAVITY_TEXT_START
+            btn.iconTint = ColorStateList.valueOf(requireContext().getColor(com.core.common.R.color.icon_kakao))
+        }
+        binding.btnEmailLogin.txtContent = getString(R.string.login_email)
     }
 
 }
