1b4ac3a63e3c96c1704508623c1a1b2a7f6d0944
PIYUJIN <yjyj020201@gmail.com>
2023-09-14 23:02:45 +0900

    Feat: 자가진단 검사시작 화면 구성 및 화면 전환 기능 구현
    [feature:selfcheck:ui] 자가진단 검사시작 toolbar 구성 및 화면 전환 기능 구현
    
    Fixes: #9

--- feature/selfcheck/ui/src/main/java/com/kusitms/hdmedi/feature/selfcheck/ui/SelfCheckFragment.kt
+++ feature/selfcheck/ui/src/main/java/com/kusitms/hdmedi/feature/selfcheck/ui/SelfCheckFragment.kt
@@ -5,55 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.kusitms.hdmedi.feature.selfcheck.ui.databinding.FragmentSelfCheckBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SelfCheckFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SelfCheckFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var fragmentSelfCheckBinding: FragmentSelfCheckBinding
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_self_check, container, false)
+
+        fragmentSelfCheckBinding = FragmentSelfCheckBinding.inflate(inflater)
+
+        fragmentSelfCheckBinding.run {
+            toolbarSelfCheck.run {
+                title = "자가진단"
+            }
+        }
+        return fragmentSelfCheckBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SelfCheckFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SelfCheckFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        fragmentSelfCheckBinding.run {
+            buttonSelfCheckStart.setOnClickListener {
+                navController.navigate(R.id.action_selfCheckFragment_to_adhdFragment)
             }
+        }
     }
 }
