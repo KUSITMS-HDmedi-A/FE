7ea2cfa66c848db3dcc4b766a5354e8f7c77b7a2
Minju Kim <mindori1019@gmail.com>
2023-09-14 16:54:39 +0900

    feature: baseFragment 상속받은 SinginFragment 생성

---
+++ feature/signin/ui/src/main/java/com/kusitms/hdmedi/feature/signin/ui/SigninFragment.kt
@@ -0,0 +1,14 @@
+package com.kusitms.hdmedi.feature.signin.ui
+
+import com.core.common.BaseFragment
+import com.kusitms.hdmedi.feature.signin.ui.databinding.FragmentSigninBinding
+
+class SigninFragment : BaseFragment<FragmentSigninBinding>(R.layout.fragment_signin) {
+    override fun createView(binding: FragmentSigninBinding) {
+    }
+
+    override fun viewCreated() {
+
+    }
+
+}
