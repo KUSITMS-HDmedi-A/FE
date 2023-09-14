bedfd316fcfcf44695b3bfc36ffe3f5f3ef003c2
Minju Kim <mindori1019@gmail.com>
2023-09-15 04:21:42 +0900

    feature: kakao 로그인 구현

--- feature/signin/ui/src/main/java/com/kusitms/hdmedi/feature/signin/ui/SigninFragment.kt
+++ feature/signin/ui/src/main/java/com/kusitms/hdmedi/feature/signin/ui/SigninFragment.kt
@@ -5,17 +5,21 @@ import android.util.Log
 import com.core.common.BaseFragment
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.snackbar.Snackbar
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.user.UserApiClient
 import com.kusitms.hdmedi.core.navigation.NavigationGraphFlow
 import com.kusitms.hdmedi.core.navigation.ToNavGraph
 import com.kusitms.hdmedi.feature.signin.ui.databinding.FragmentSigninBinding
 
 class SigninFragment : BaseFragment<FragmentSigninBinding>(R.layout.fragment_signin) {
+
     override fun createView(binding: FragmentSigninBinding) {
     }
 
     override fun viewCreated() {
         binding.lifecycleOwner = this
         initView()
+        initClickListener()
     }
 
     private fun initView() {
@@ -29,12 +33,46 @@ class SigninFragment : BaseFragment<FragmentSigninBinding>(R.layout.fragment_sig
             btn.iconTint = ColorStateList.valueOf(requireContext().getColor(com.core.common.R.color.icon_kakao))
         }
         binding.btnEmailLogin.txtContent = getString(R.string.login_email)
+    }
 
+    private fun initClickListener(){
         binding.btnEmailLogin.btn.setOnClickListener {
-            (requireActivity() as ToNavGraph).navigateToGraph(NavigationGraphFlow.BottomGraphFlow)
+            navigateToBottomFlow()
             Snackbar.make(requireContext(), it, "Click!", Snackbar.LENGTH_SHORT).show()
-            Log.d("Signin", "clcik email btn")
         }
+
+        binding.btnKakaoLogin.btn.setOnClickListener {
+            kakaoLogin { isSuccess, msg ->
+                if (isSuccess) {
+                    Log.i(requireContext().toString(), "카카오 로그인 성공 $msg")
+                    successLogin(msg)
+                }
+                else Log.e(requireContext().toString(), "카카오 로그인 실패 : $msg")
+            }
+        }
+    }
+
+    private fun kakaoLogin(isSuccess: (Boolean, String) -> Unit) {
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
+            UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
+                if (error != null) isSuccess(false, error.toString())
+                else if (token != null) isSuccess(true, token.accessToken)
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(requireContext()) { token, error ->
+                if (error != null) isSuccess(false, error.toString())
+                else if (token != null) isSuccess(true, token.accessToken)
+            }
+        }
+    }
+
+    private fun successLogin(token: String ){
+        //viewModel.setUserAccessToken(token)
+        navigateToBottomFlow()
+    }
+
+    private fun navigateToBottomFlow(){
+        (requireActivity() as ToNavGraph).navigateToGraph(NavigationGraphFlow.BottomGraphFlow)
     }
 
 }
