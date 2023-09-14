f028221bff1672e79dddefc5bed62026c973e524
Minju Kim <mindori1019@gmail.com>
2023-09-15 04:21:29 +0900

    feature: kakao 로그인 init 작업

--- app/src/main/java/com/kusitms/hdmedi/HDmediApplication.kt
+++ app/src/main/java/com/kusitms/hdmedi/HDmediApplication.kt
@@ -1,8 +1,14 @@
 package com.kusitms.hdmedi
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class HDmediApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        KakaoSdk.init(this, "f195ae8c23b92631efab4b034c32bc85")
+    }
 }
