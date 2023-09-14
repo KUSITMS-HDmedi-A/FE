8f1676f85c01f1a6553fc3aec019cdb659d4fd19
PIYUJIN <yjyj020201@gmail.com>
2023-09-14 23:11:05 +0900

    Feat: 자가진단 결과 화면 구성 및 화면 전환 기능 구현
    [feature:selfcheck:ui] 자가진단 결과 화면 toolbar 및 메뉴 구성, 화면 전환 기능 구현
    
    Fixes: #9

---
+++ feature/selfcheck/ui/src/main/java/com/kusitms/hdmedi/feature/selfcheck/ui/SelfCheckResultFragment.kt
@@ -0,0 +1,51 @@
+package com.kusitms.hdmedi.feature.selfcheck.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.kusitms.hdmedi.feature.selfcheck.ui.databinding.FragmentSelfCheckResultBinding
+
+class SelfCheckResultFragment : Fragment() {
+
+    lateinit var fragmentSelfCheckResultBinding: FragmentSelfCheckResultBinding
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        fragmentSelfCheckResultBinding = FragmentSelfCheckResultBinding.inflate(inflater)
+
+        fragmentSelfCheckResultBinding.run {
+            toolbarSelfCheckResult.run {
+                title = "자가진단"
+
+                inflateMenu(R.menu.close_menu)
+
+                setOnMenuItemClickListener {
+                    when (it.itemId) {
+                        R.id.item_close -> {
+                            navController.navigate(R.id.action_selfCheckResultFragment_to_selfCheckFragment)
+                        }
+                        else -> { }
+                    }
+                    true
+                }
+
+            }
+        }
+        return fragmentSelfCheckResultBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+    }
+}
