823a65aeae8ebd2f3bceb18888d542930a45ae10
Minju Kim <mindori1019@gmail.com>
2023-09-15 15:50:28 +0900

    feature: repository di 작업 (인터페이스와 구현체)

---
+++ feature/signin/data/src/main/java/com/kusitms/hdmedi/feature/signin/data/di/DataLayerModule.kt
@@ -0,0 +1,33 @@
+package com.kusitms.hdmedi.feature.signin.data.di
+
+import com.core.network.datasource.NetworkDataSource
+import com.core.network.utils.TokenDataStore
+import com.kusitms.hdmedi.feature.signin.data.repo.SigninRepositoryImpl
+import com.kusitms.hdmedi.feature.signin.domain.repo.SigninRepository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class DataLayerModule {
+
+    @Binds
+    abstract fun bindsSigninRepository(
+        repository: SigninRepositoryImpl
+    ): SigninRepository
+
+    @InstallIn(SingletonComponent::class)
+    @Module
+    internal object FakeModule {
+        @Singleton
+        fun provideSigninRepo(
+            dataSource: NetworkDataSource,
+            tokenDataStore: TokenDataStore
+        ): SigninRepository =
+            SigninRepositoryImpl(dataSource = dataSource, tokenSource = tokenDataStore)
+    }
+}
