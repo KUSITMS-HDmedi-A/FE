4c024c588afa3505f63d6c95d3f5b3c6f5c69a21
Minju Kim <mindori1019@gmail.com>
2023-09-15 17:56:59 +0900

    build: firebase messaging 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,6 @@ plugins {
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.com.android.library) apply false
     alias(libs.plugins.com.google.dagger.hilt.android) apply false
+    alias(libs.plugins.com.google.gms.google.services) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
