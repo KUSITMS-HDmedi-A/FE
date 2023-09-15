6440b13271394ad40c25dc1ea7dd07fdd51480ae
Minju Kim <mindori1019@gmail.com>
2023-09-16 00:50:58 +0900

    style: fragment_create_alarm.xml 생성

---
+++ feature/home/ui/src/main/java/com/kusitms/hdmedi/feature/home/ui/CreateAlarmFragment.kt
@@ -0,0 +1,20 @@
+package com.kusitms.hdmedi.feature.home.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.core.common.BaseFragment
+import com.kusitms.hdmedi.feature.home.ui.databinding.FragmentCreateAlarmBinding
+
+class CreateAlarmFragment : BaseFragment<FragmentCreateAlarmBinding>(R.layout.fragment_create_alarm) {
+    override fun createView(binding: FragmentCreateAlarmBinding) {
+        TODO("Not yet implemented")
+    }
+
+    override fun viewCreated() {
+        TODO("Not yet implemented")
+    }
+
+}
