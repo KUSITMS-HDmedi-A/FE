5489f94f3839855f968a5e3e47e2b1bdbe06a118
Minju Kim <mindori1019@gmail.com>
2023-09-16 02:51:34 +0900

    style: 알람 추가 화면 xml 대략적으로 구성

--- feature/home/ui/src/main/java/com/kusitms/hdmedi/feature/home/ui/CreateAlarmFragment.kt
+++ feature/home/ui/src/main/java/com/kusitms/hdmedi/feature/home/ui/CreateAlarmFragment.kt
@@ -1,22 +1,38 @@
 package com.kusitms.hdmedi.feature.home.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.core.common.BaseFragment
 import com.kusitms.hdmedi.feature.home.ui.databinding.FragmentCreateAlarmBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CreateAlarmFragment : BaseFragment<FragmentCreateAlarmBinding>(R.layout.fragment_create_alarm) {
+class CreateAlarmFragment :
+    BaseFragment<FragmentCreateAlarmBinding>(R.layout.fragment_create_alarm) {
     override fun createView(binding: FragmentCreateAlarmBinding) {
 
     }
 
     override fun viewCreated() {
+        binding.lifecycleOwner = this
+        initView()
+    }
 
+    private fun initView() {
+        binding.header.headerTitle = getString(R.string.add_alarm)
+
+        val itemList = listOf("타이레놀", "소화제", "김기약", "해열제")
+        val itemAdapter: ArrayAdapter<String> =
+            ArrayAdapter(requireContext(), R.layout.item_spinner, itemList)
+        binding.autoTv.setAdapter(itemAdapter)
+        binding.autoTv.setOnItemClickListener { parent, view, position, id ->
+            Log.d(javaClass.name, "${itemAdapter.getItemId(position)}")
+        }
     }
 
 }
