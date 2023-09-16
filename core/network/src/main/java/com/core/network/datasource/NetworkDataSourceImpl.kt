185ccbe757434965fba4dd8002012e8e40058bfd
PIYUJIN <yjyj020201@gmail.com>
2023-09-17 04:53:05 +0900

    Feat: NetworkDataSourceImpl에 함수 구현
    [core:network] NetworkDataSourceImpl에 함수 구현
    
    Fixes: #9

--- core/network/src/main/java/com/core/network/datasource/NetworkDataSourceImpl.kt
+++ core/network/src/main/java/com/core/network/datasource/NetworkDataSourceImpl.kt
@@ -1,15 +1,20 @@
 package com.core.network.datasource
 
 import com.core.network.ApiService
+import com.core.network.model.ADHDQuestionResponse
 import com.core.network.model.EnrollMedicineRequest
 import com.core.network.model.MedicineListResponse
 import com.core.network.model.Response
 import com.core.network.model.TokenResponse
+import retrofit2.Call
 import javax.inject.Inject
 
 class NetworkDataSourceImpl @Inject constructor(
     private val networkApi: ApiService
 ) : NetworkDataSource {
+
+    override fun getAdhdQuestion(token: String): Call<ADHDQuestionResponse> = networkApi.getAdhdQuestion(token = token)
+
     override suspend fun kakaoLogin(): TokenResponse = networkApi.kakaoLogin()
 
 
