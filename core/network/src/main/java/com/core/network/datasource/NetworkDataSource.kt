47e64699fc3de48335c0ad64b40213930235a19c
PIYUJIN <yjyj020201@gmail.com>
2023-09-17 04:52:32 +0900

    Feat: NetworkDataSource에 함수 추가 (Interface)
    [core:network] NetworkDataSource에 함수 추가 (Interface)
    
    Fixes: #9

--- core/network/src/main/java/com/core/network/datasource/NetworkDataSource.kt
+++ core/network/src/main/java/com/core/network/datasource/NetworkDataSource.kt
@@ -1,14 +1,18 @@
 package com.core.network.datasource
 
+import com.core.network.model.ADHDQuestionResponse
 import com.core.network.model.EnrollMedicineRequest
 import com.core.network.model.MedicineListResponse
 import com.core.network.model.Response
 import com.core.network.model.TokenResponse
+import retrofit2.Call
+import retrofit2.http.Header
 
 interface NetworkDataSource {
     /**
      * adhd 진단
      */
+    fun getAdhdQuestion(@Header("Authorization") token: String) : Call<ADHDQuestionResponse>
 
     /**
      * auth (회원)
