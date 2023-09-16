8bdd9136779bb15c79bb69e388f88b574ce3a6d1
PIYUJIN <yjyj020201@gmail.com>
2023-09-17 04:49:12 +0900

    Feat: ApiService에 자가진단 api 추가
    [core:network] ApiService에 자가진단(질문, 결과) api 추가
    
    Fixes: #9

--- core/network/src/main/java/com/core/network/ApiService.kt
+++ core/network/src/main/java/com/core/network/ApiService.kt
@@ -1,17 +1,27 @@
 package com.core.network
 
+import com.core.network.model.ADHDQuestionResponse
+import com.core.network.model.ADHDResultRequest
+import com.core.network.model.ADHDResultResponse
 import com.core.network.model.EnrollMedicineRequest
 import com.core.network.model.MedicineListResponse
 import com.core.network.model.Response
 import com.core.network.model.TokenResponse
+import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
     /**
      * adhd 진단
      */
+    @GET("/api/adhd/test")
+    fun getAdhdQuestion(@Header("Authorization") token: String) : Call<ADHDQuestionResponse>
+
+    @POST("/api/adhd/test/result")
+    fun sendAdhdResult(@Header("Authorization") token: String, @Body result: ADHDResultRequest): Call<ADHDResultResponse>
 
     /**
      * auth (회원)
