783e4af1e11179c10534a8fc034aa4f80c15e76d
Minju Kim <mindori1019@gmail.com>
2023-09-17 05:57:30 +0900

    refactor: create_alarm_fragment ui 개선

--- feature/home/ui/src/main/java/com/kusitms/hdmedi/feature/home/ui/CreateAlarmFragment.kt
+++ feature/home/ui/src/main/java/com/kusitms/hdmedi/feature/home/ui/CreateAlarmFragment.kt
@@ -8,7 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.core.common.BaseFragment
+import com.core.common.adapter.ProfileAdapter
+import com.core.common.model.Profile
 import com.kusitms.hdmedi.feature.home.ui.databinding.FragmentCreateAlarmBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -32,10 +35,17 @@ class CreateAlarmFragment :
         binding.header.apply {
             headerTitle = getString(R.string.add_alarm)
             rightTxt = getString(R.string.enroll)
+            btnBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
         }
 
         // 사람 선택
-        //todo
+        val profileList = profileList()
+        binding.rvPeople.rv.adapter = ProfileAdapter(profileList) { clickedProfile ->
+            Log.d(javaClass.name, "clicked : ${clickedProfile}")
+            viewModel.updateSelectedName(clickedProfile.name!!)
+        }
 
         // 약 선택
         val itemList = listOf("타이레놀", "소화제", "김기약", "해열제")
@@ -54,5 +64,23 @@ class CreateAlarmFragment :
         binding.btnWeekday.tvTitle.text = getString(R.string.weekday)
         binding.btnWeekend.tvTitle.text = getString(R.string.weekend)
     }
+
+    private fun profileList(): MutableList<Profile> = mutableListOf(
+        Profile(
+            name = "나(엄마)",
+            img = requireContext().getDrawable(com.core.common.R.drawable.img_mom)!!,
+            selected = true
+        ),
+        Profile(
+            name = "김리아(딸)",
+            img = requireContext().getDrawable(com.core.common.R.drawable.img_daughter)!!,
+            selected = false
+        ),
+        Profile(
+            name = "김리준(아들)",
+            img = requireContext().getDrawable(com.core.common.R.drawable.img_son)!!,
+            selected = false
+        ),
+    )
 
 }
--- feature/home/ui/src/main/java/com/kusitms/hdmedi/feature/home/ui/CreateAlarmViewModel.kt
+++ feature/home/ui/src/main/java/com/kusitms/hdmedi/feature/home/ui/CreateAlarmViewModel.kt
@@ -1,5 +1,6 @@
 package com.kusitms.hdmedi.feature.home.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.kusitms.hdmedi.feature.home.domain.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,5 +11,12 @@ import javax.inject.Inject
 class CreateAlarmViewModel @Inject constructor(
     repository: HomeRepository
 ): ViewModel() {
+
+    private val _selectedName = MutableLiveData<String>()
+    val selectedName get() = _selectedName
+
+    fun updateSelectedName(name: String){
+        _selectedName.value = name
+    }
 
 }
