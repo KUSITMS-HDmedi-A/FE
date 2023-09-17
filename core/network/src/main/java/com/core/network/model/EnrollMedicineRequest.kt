a7fb2702f132b1f0ad38386af01b4ccf02694664
PIYUJIN <yjyj020201@gmail.com>
2023-09-17 12:45:46 +0900

    Feat: 약 등록 data class 생성
    [core:network] 약 등록 request data class 생성
    
    Fixes: #4

--- core/network/src/main/java/com/core/network/model/EnrollMedicineRequest.kt
+++ core/network/src/main/java/com/core/network/model/EnrollMedicineRequest.kt
@@ -5,9 +5,9 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class EnrollMedicineRequest(
-    val characterName: String,
-    val endDate: String,
-    val medicine: List<String>,
     val purpose: String,
-    val startDate: String
+    val startDate: String,
+    val endDate: String,
+    val characterName: String,
+    val medicine: List<String>
 )
