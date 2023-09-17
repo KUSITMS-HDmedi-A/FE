937eb33c780e2bdf9a833f39276e5ca26508e14c
PIYUJIN <yjyj020201@gmail.com>
2023-09-17 16:43:52 +0900

    Build: version 수정 및 라이브러리 추가
    compile jdk version error로 17로 수정 및 라이브러리 추가

--- feature/search/ui/build.gradle.kts
+++ feature/search/ui/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
 
@@ -41,9 +41,14 @@ android {
 
 dependencies {
 
+    implementation(project(":core:common"))
+    implementation(project(":core:navigation"))
+
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.navigation)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
